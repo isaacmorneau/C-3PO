d7db152044c1ba41704b43e520fb2e15804f4881
Isaac Morneau <isaacmorneau@gmail.com>
2019-01-30 20:50:55 -0800

    better example

--- src/test.c
+++ src/test.c
@@ -9,9 +9,16 @@ int main(void) {
     C3PO_STR(printf("decoded '%s'\n", c3po_str), BSTR);
 #pragma C3PO option
     C3PO_STR(printf("decoded '%s'\n", c3po_str), CSTR);
-
 #pragma C3PO option
     puts(HELLOWORLD);
+#pragma C3PO option
+
+#pragma C3PO garbage
+    puts(HELLOWORLD);
+#pragma C3PO garbage
+    puts(HELLOWORLD);
+#pragma C3PO garbage
+
 #pragma C3PO end
     return 0;
 }
