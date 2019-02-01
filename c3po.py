8369ce11ad9cf0165ee28da300ea0d579affe827
Isaac Morneau <isaacmorneau@gmail.com>
2019-01-31 22:31:20 -0800

    fixed broken asm generation

--- c3po.py
+++ c3po.py
@@ -27,8 +27,8 @@ asmlbl = """
 asmlbljmp = """
     __asm__(
         "xor %%eax, %%eax;"
-        "cmp %%eax, {0};"
-        "jl .done{0};"
+        "test %%eax, %%eax;"
+        "jz .done{0};"
         "call .shatter{1};"
         ".done{0}:"
         :::"%eax");
@@ -206,58 +206,6 @@ class Line():
         else:
             file.write(self.line)
 
-
-def line_cxor(lines):
-    parsedlines = []
-    enabled = False
-    for line in lines:
-        cleanline = line.strip()
-
-        #check for the pragmas
-        if cxor_enable_string.match(cleanline):
-            if enabled:
-                print("Duplicate cxor enable pragma found", file=sys.stderr)
-            enabled = True
-            continue
-        elif cxor_disable_string.match(cleanline):
-            if not enabled:
-                print("Duplicate cxor disable pragma found", file=sys.stderr)
-            enabled = False
-            continue
-
-        #dont check when its not on
-        if not enabled:
-            parsedlines.append(line)
-            continue
-
-        #check if this is something to change
-        if simplestring.match(cleanline):
-            parts = simplestring.search(cleanline)
-            varname = parts.group(1)
-            original = parts.group(2)
-            #magic to parse escapes
-            #thanks jerub https://stackoverflow.com/a/4020824
-            string = bytes(original, "utf-8").decode("unicode_escape").encode()
-            encarray = list(secrets.token_bytes(len(string)))
-            newarray = []
-            for i in range(len(string)):
-                newarray.append(encarray[i] ^ string[i])
-
-            newarray.append(0)
-            encarray.append(0)
-            parsedlines.append("""//#define {1} "{0}"
-#define {1}_ENC {{{3},{4}}}
-#define {1}_LEN {2}
-""".format(original, varname, len(newarray), ','.join(hex(e) for e in newarray), ','.join(hex(e) for e in encarray)))
-        else:
-            #maintain unchanged lines
-            parsedlines.append(line)
-
-    #check for bad formatting
-    if enabled:
-        print("Cxor enable pragma never closed", file=sys.stderr)
-    return parsedlines
-
 def line_shuffle(lines):
     parsedlines = []
     shuffle_sets = [[]]
@@ -305,63 +253,6 @@ def line_shuffle(lines):
                 parsedlines.append(s)
 
     return parsedlines
-
-def line_garbage(lines):
-    parsedlines = []
-    enabled = False
-    shatterer = 2
-    labelnum = 0
-    linenum = 0
-    for line in lines:
-        cleanline = line.strip()
-
-        #check for the pragmas
-        if shatter_enable_string.match(cleanline):
-            if enabled:
-                print("Duplicate shatter enable pragma found", file=sys.stderr)
-            enabled = True
-            linenum = 0
-            parts = shatter_enable_string.search(cleanline)
-            level = parts.group(1)
-            if level == " low":
-                shatterer = 3
-            elif level == " high":
-                shatterer = 1
-            #elif level == " medium":
-            else:
-                shatterer = 2
-            continue
-        elif shatter_disable_string.match(cleanline):
-            if not enabled:
-                print("Duplicate shatter disable pragma found", file=sys.stderr)
-            enabled = False
-            continue
-
-        #dont check when its not on
-        #just pass the lines through
-        if not enabled:
-            parsedlines.append(line)
-            continue
-
-        #if garbagestring.match(cleanline):
-        if linenum % shatterer == 0:
-            #requesting
-            labelnum += 1
-            parsedlines.append(asmlbl.format(labelnum))
-            parsedlines.append(asmlbljmp.format(labelnum, secrets.randbelow(labelnum) + 1))
-        elif enabled:
-            labelnum += 1
-            parsedlines.append(asmlbl.format(labelnum))
-
-        parsedlines.append(line)
-        #counter for interspacing shatter
-        linenum += 1
-
-    if enabled:
-        print("Shatter enable pragma never closed", file=sys.stderr)
-
-    return parsedlines
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
