fe63b1612d715337c1350bcf4801f66b0fd5d848
Isaac Morneau <isaacmorneau@gmail.com>
2019-02-13 22:06:21 -0800

    made the testing not silent

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 #unit tests
+echo "==> unit tests"
 ./c3po.py test
 #integration tests
+echo "==> preparing for integration tests"
 ./c3po.py test/src test/gen/ c823d57855
 
+echo "==> integration tests"
 for f in test/gen/*.c; do
+    echo "diffing $f and $f.valid"
     diff -qy "$f" "$f.valid"
 done
+echo "==> complete"
+
