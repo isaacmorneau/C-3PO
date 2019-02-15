0826ff25d818472da31020dfcb17bec7db10a723
Isaac Morneau <isaacmorneau@gmail.com>
2019-02-15 09:34:32 -0800

    added valid integration tests

--- test/src/cxor.c
+++ test/src/cxor.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 
 #pragma c3po cxor(on, 32)
@@ -9,5 +10,23 @@
 #pragma c3po cxor(off)
 
 int main(void) {
+    {
+        const size_t c3po_len = hw_LEN;
+        char c3po_str[c3po_len];
+        uint8_t c3po_enc[]  = hw_ENC;
+        uint8_t c3po_key1[] = hw_KEY1;
+        uint8_t c3po_key2[] = hw_KEY2;
+        c3po_str_xor(c3po_enc, c3po_key1, c3po_key2, c3po_str, c3po_len);
+        puts(c3po_str);
+    }
+    {
+        const size_t c3po_len = em_LEN;
+        char c3po_str[c3po_len];
+        uint8_t c3po_enc[]  = em_ENC;
+        uint8_t c3po_key1[] = em_KEY1;
+        uint8_t c3po_key2[] = em_KEY2;
+        c3po_str_xor(c3po_enc, c3po_key1, c3po_key2, c3po_str, c3po_len);
+        puts(c3po_str);
+    }
     return 0;
 }
--- test/src/shatter.c
+++ test/src/shatter.c
@@ -5,6 +5,8 @@ int main(void) {
     puts("testing");
     puts("shatter");
     puts("graphs");
+#pragma c3po shatter(off)
+#pragma c3po shatter(call, high, on)
     puts("1");
     puts("2");
     puts("3");
