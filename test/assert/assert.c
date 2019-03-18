d3188d97b99b65bad438cefcd01318e424fbe34c
Isaac Morneau <isaacmorneau@gmail.com>
2019-03-18 12:51:55 -0700

    made the call graph a better example

--- test/assert/assert.c
+++ test/assert/assert.c
@@ -3,7 +3,10 @@
 int main(int argc, char **argv) {
     (void)argv;
 #pragma c3po assert(argc > 0)
+
     for (int i = 0; i < 10; ++i) {
+#pragma c3po assert(i < 10)
         printf("%d", i);
+#pragma c3po assert(i >= 0)
     }
 }
